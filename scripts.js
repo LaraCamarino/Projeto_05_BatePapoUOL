2838ee9e30fcd0e99329e7e81f16ac4446367dba
laracamarino <laracamarino@gmail.com>
2022-04-17 19:25:11 -0300

    Versão 3 - Chat Implementado

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let nomeUsuario;
+let mensagens = [];
 
 perguntarUsuario()
 
@@ -9,7 +10,7 @@ function perguntarUsuario() {
     }
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nome);
    
-    
+    promise.then(pegarMensagens);
     promise.catch(erroEntrada);
 }
 
@@ -21,6 +22,66 @@ function erroEntrada(error) {
     }
 }
 
+function pegarMensagens() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    
+    promise.then(carregarMensagens);
+}
+
+function carregarMensagens(response) {
+    mensagens = response.data;
+    renderizarMensagens();
+}
+
+function renderizarMensagens() {
+    const caixaMensagens = document.querySelector(".caixa-mensagens");
+    caixaMensagens.innerHTML = "";
+
+    for(let i = 0; i < mensagens.length; i++) {
+        if(mensagens[i].type === "status") {
+            caixaMensagens.innerHTML += `
+            <div class="mensagem status">
+                <span class="hora">(${mensagens[i].time})</span>
+                <span class="nome-usuario">${mensagens[i].from}</span>
+                <span>${mensagens[i].text}</span>
+            </div>
+        `;
+        }
+        else if(mensagens[i].type === "message") {
+            caixaMensagens.innerHTML += `
+            <div class="mensagem">
+                <span class="hora">(${mensagens[i].time})</span>
+                <span class="nome-usuario">${mensagens[i].from}</span>
+                <span>para</span>
+                <span class="nome-usuario">${mensagens[i].to}:</span>
+                <span class="conteudo-mensagem">${mensagens[i].text}</span>
+            </div>
+        `;
+        }
+        else if(mensagens[i].type === "private_message") {
+            if(mensagens[i].from === nomeUsuario || mensagens[i].to === nomeUsuario) {
+                caixaMensagens.innerHTML += `
+                <div class="mensagem reservada">
+                    <span class="hora">(${mensagens[i].time})</span>
+                    <span class="nome-usuario">${mensagens[i].from}</span>
+                    <span>reservadamente para</span>
+                    <span class="nome-usuario">${mensagens[i].to}:</span>
+                    <span class="conteudo-mensagem">${mensagens[i].text}</span>
+                </div>
+            `;
+            }
+        }
+    }
+    scrollAutomatico();
+}
+
+function scrollAutomatico() {
+    const caixaMensagens = document.querySelector(".caixa-mensagens");
+    const ultimaMensagem = caixaMensagens.lastElementChild;
+    ultimaMensagem.scrollIntoView();
+}
+
+setInterval(pegarMensagens, 3000);
 setInterval(continuarLogado, 4000);
 
 function continuarLogado() {
